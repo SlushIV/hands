2a75f1657920b0d2269158f56bffed4658f2dcbf
Bojun <thechosenone43728@gmail.com>
2026-03-30 21:43:22 +0200

    added pinch using distance < 20 check and added play/pause, skip and prev functionaity based on where on screen the pinch happened

--- main.py
+++ main.py
@@ -2,12 +2,17 @@ import cv2
 import mediapipe as mp
 import numpy as np
 import math
+import pyautogui
+import time
 
 mp_hands = mp.solutions.hands
 hands = mp_hands.Hands(min_detection_confidence=0.8, min_tracking_confidence=0.5)
 camera = cv2.VideoCapture(0)
 
-canvas = None
+last_action_time = 0      
+action_cooldown = 1.0     
+ripples = []              
+current_action = "Waiting for gesture..."
 
 while camera.isOpened():
     success, frame = camera.read()
@@ -15,11 +20,14 @@ while camera.isOpened():
 
     frame = cv2.flip(frame, 1) # horizontal flip so frame matches mirror view
     height, width, _ = frame.shape
-    if canvas is None: canvas = np.zeros_like(frame) # new blank (black) canvas
     
     # BGR to RGB conversion for mediapipe to work
     rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
     results = hands.process(rgb_frame)
+
+    # Add HUD Text
+    cv2.putText(frame, f"Status: {current_action}", (width // 2 - 100, 30), 
+                cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0, 0, 0), 1)
 
     if results.multi_hand_landmarks and results.multi_handedness:
         for fingers, handedness in zip(results.multi_hand_landmarks, results.multi_handedness):
@@ -54,13 +62,45 @@ while camera.isOpened():
             # handedness display
             cv2.putText(frame, f"Hand: {label}", (thumbX, thumbY + 30), 
                         cv2.FONT_HERSHEY_SIMPLEX, 0.6, (255, 255, 255), 2)
+            
+            current_time = time.time()
+            
+            # pinch fingers
+            if distance < 20 and (current_time - last_action_time) > action_cooldown and label == "Right":
+                
+                if indexX > width * 0.7:  # hand is on the RIGHT side
+                    pyautogui.press('nexttrack')
+                    current_action = "Next Track ->"
+                    ripples.append([indexX, indexY, 5]) # Spawn a ripple
+                    last_action_time = current_time
+                    
+                elif indexX < width * 0.3: # hand is on the LEFT side
+                    pyautogui.press('prevtrack')
+                    current_action = "<- Previous Track"
+                    ripples.append([indexX, indexY, 5]) 
+                    last_action_time = current_time
+                    
+                else: # hand is in the MIDDLE
+                    pyautogui.press('playpause')
+                    current_action = "Play / Pause"
+                    ripples.append([indexX, indexY, 5]) 
+                    last_action_time = current_time
+
+    for r in ripples[:]: # iterate over a copy of the list
+        rx, ry, radius = r[0], r[1], r[2]
+        
+        cv2.circle(frame, (rx, ry), radius, (255, 255, 255), 2) 
+        # expand radius
+        r[2] += 8 
+    
+        if r[2] > 150:
+            ripples.remove(r)
 
     cv2.imshow("hands", frame)
 
     # key input
     key = cv2.waitKey(1) & 0xFF
     if key == ord('q'): break
-    elif key == ord('c'): canvas = np.zeros_like(frame)
     elif key in [ord('1'), ord('2'), ord('3')]:
         mode = int(chr(key))
         points = []
