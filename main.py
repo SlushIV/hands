dacc8b569e9c05f8c67e1504aec0843fe1e3bde4
Bojun <thechosenone43728@gmail.com>
2026-03-30 20:56:21 +0200

    added handiness that can detect if the hand is left or right which is also desplayed next to the fingers, also color based on right/left hand

--- main.py
+++ main.py
@@ -21,10 +21,14 @@ while camera.isOpened():
     rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
     results = hands.process(rgb_frame)
 
-    if results.multi_hand_landmarks:
-        for fingers in results.multi_hand_landmarks:
+    if results.multi_hand_landmarks and results.multi_handedness:
+        for fingers, handedness in zip(results.multi_hand_landmarks, results.multi_handedness):
+
             index = fingers.landmark[8]
             thumb = fingers.landmark[4]
+
+            # "Right" or "Left" hand label
+            label = handedness.classification[0].label 
             
             indexX, indexY = int(index.x * width), int(index.y * height)
             thumbX, thumbY = int(thumb.x * width), int(thumb.y * height)
@@ -32,19 +36,23 @@ while camera.isOpened():
             # distance between index and thumb
             distance = math.hypot(indexX - thumbX, indexY - thumbY)
 
+            color = (0, 255, 0) if label == "Right" else (0, 0, 255) # green for right hand, red for left hand
+
             # draw line between fingers
-            line_color = (0, 255, 0)
-            cv2.line(frame, (indexX, indexY), (thumbX, thumbY), line_color, 3)
+            cv2.line(frame, (indexX, indexY), (thumbX, thumbY), color, 3)
 
             # draw circles at fingertips
-            cv2.circle(frame, (indexX, indexY), 10, (255, 255, 255), -1)
-            cv2.circle(frame, (indexX, indexY), 15, (255, 0, 255), 2)
-            
+            cv2.circle(frame, (indexX, indexY), 10, (255, 255, 255), -1) # inner white circle
+            cv2.circle(frame, (indexX, indexY), 15, color, 2) # outer circle  
             cv2.circle(frame, (thumbX, thumbY), 10, (255, 255, 255), -1)
-            cv2.circle(frame, (thumbX, thumbY), 15, (255, 0, 255), 2)
+            cv2.circle(frame, (thumbX, thumbY), 15, color, 2)
 
             # distance text display
             cv2.putText(frame, f"Distance: {int(distance)}", (indexX, indexY - 20), 
+                        cv2.FONT_HERSHEY_SIMPLEX, 0.6, (255, 255, 255), 2)
+            
+            # handedness display
+            cv2.putText(frame, f"Hand: {label}", (thumbX, thumbY + 30), 
                         cv2.FONT_HERSHEY_SIMPLEX, 0.6, (255, 255, 255), 2)
 
     cv2.imshow("hands", frame)
